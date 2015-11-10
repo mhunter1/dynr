c7daa29903e030f06ce244cd7562da0cbbfc6fd9
symiin <symiin@psu.edu>
2015-11-10 08:14:25 -0500

    Demo uses new specification for RSmodel.c; debugging dynrRun.R

--- demo/demoRSlinearODE.R
+++ demo/demoRSlinearODE.R
@@ -1,7 +1,7 @@
 
 require(dynr)
 
-thedata = read.table('./data/New2CovModel1T1000n20batch1ODEsimData.txt')
+thedata = read.table('./data/New2CovT200n10batch1ODEsimData.txt')
 thedata$V6 <- as.numeric(thedata$V6)
 data <- dynr.data(thedata, id="V1", time="V2",observed=paste0('V', 3:4), 
                   covariates=paste0('V', 5:6))
@@ -10,22 +10,37 @@ data <- dynr.data(thedata, id="V1", time="V2",observed=paste0('V', 3:4),
 model <- dynr.model(
               num_regime=2,
               dim_latent_var=2,
-              xstart=c(rep(log(.1), 4), log(10.0), log(10.0), -3.0, 9.0, -1.5, -0.5, 95.0,-25,-.3),
-              ub=c(rep(10, 6), rep(10, 4), 200, 10, 10),
-              lb=c(rep(-10, 6), rep(-10, 4), 0, -30, -10),
-	     options=list(maxtime=60*60, maxeval=1000)
+              xstart=c(rep(log(.1), 4), 95.0, log(10.0), log(10.0), -3.0, 9.0, -1.5, -0.5, -1,-.3),
+              ub=c(rep(1.5, 4), 200, 5, 5, rep(10, 6)),
+              lb=c(rep(-10, 4), 0, -10, -10, rep(-10, 6)),
+	     options=list(maxtime=.0001*60, maxeval=2)
 )
 
-func_address=dynr.funcaddresses(file="./demo/RSODEmodel.c",verbose=FALSE)
-tfun <- function(x){c(exp(x[1:6]), x[7:13])}
+
+func_noise_cov_txt="void function_noise_cov(size_t t, size_t regime, double *param, gsl_matrix *y_noise_cov, gsl_matrix *eta_noise_cov){
+size_t i;
+for (i=0;i<eta_noise_cov->size1;i++){
+gsl_matrix_set(eta_noise_cov,i,i,-20);
+}
+gsl_matrix_set(y_noise_cov,0,0, param[5]);
+gsl_matrix_set(y_noise_cov,1,1, param[6]);
+
+}
+" 
+
+func_address=dynr.funcaddresses(file="~/Dropbox/Symiin_Lu/dynr/demo/RSODEmodel.c",verbose=FALSE)
+tfun <- function(x){c(exp(x[1:4]),x[5],exp(x[6:7]), x[8:13])}
 res <- dynr.run(model, data,func_address,tfun)
+
+
 #True values should be
-#c(log(.2), log(.1), log(.3), log(.2), log(9.0), log(9.0), -4, 8.5, -1, 1, 100, -2, -1)
-
-summary(res)
-save.image(file="./demo/RSLinearODE.RData")
-plot(res, data=data, graphingPar=list(cex.main=1, cex.axis=1, cex.lab=1.2), numSubjDemo=2)
+#c(log(.2), log(.1), log(.3), log(.2),  100, log(9.0), log(9.0), -4, 8.5, -1, 1,-2, -1)
 
 
-dynr.ggplot(res, data.dynr=data, states=c(1,2), title="Smoothed State Values", numSubjDemo=2)
+#summary(res)
+#save.image(file="./demo/RSLinearODETestBrief.RData")
+#plot(res, data=data, graphingPar=list(cex.main=1, cex.axis=1, cex.lab=1.2), numSubjDemo=2)
+
+
+#dynr.ggplot(res, data.dynr=data, states=c(1,2), title="Smoothed State Values", numSubjDemo=2)
 
