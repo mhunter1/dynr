bbea9d5371d67297667c6b565a8d18edd5f4e491
mhunter1 <mhunter@ou.edu>
2016-01-29 22:59:57 -0600

    Improve the factor loadings recipe.

--- R/dynrRecipe.R
+++ R/dynrRecipe.R
@@ -2,20 +2,39 @@
 #  and produces a C function definition that can be
 #  compiled by dynrFuncaddress.
 
-dynr.loadings <- function(map, paramNums){
-	nx <- length(unlist(map))
+# TODO add check that there is no cross loading on one of the ID variables
+
+dynr.loadings <- function(map, params, idvar){
+	if(missing(idvar)){
+		idvar <- sapply(map, '[', 1)
+	}
+	
+	allVars <- unique(unlist(map))
+	
+	nx <- length(allVars)
 	ne <- length(map)
+	
+	if(!all(idvar %in% c(names(map), unlist(map)))){
+		stop("The 'idvar' must all be either in the names of the 'map' argument or parts of the part 'map' argument.")
+	}
+	
+	paramsNeeded <- length(unlist(map)) - sum(idvar %in% allVars)
+	if(length(params) != paramsNeeded){
+		stop(paste0("Number of free parameters provided (", length(params), ") does not match number needed (", paramsNeeded, ")."))
+	}
 	
 	SOME_CONDITION_MET <- TRUE
 	SOMETHING_WITH_PARAMNUMS <- 1
 	
+	k <- 1
 	ret <- "void function_measurement(size_t t, size_t regime, double *param, const gsl_vector *eta, const gsl_vector *co_variate, gsl_matrix *Ht, gsl_vector *y){\n\n"
-	for(i in 1:nx){
-		for(j in 1:ne){
-			if(SOME_CONDITION_MET){
+	for(j in 1:ne){
+		for(i in 1:nx){
+			if(allVars[i] %in% map[[j]] & !(allVars[i] %in% idvar)){
 				ret <- paste(ret,
 					'    gsl_matrix_set(Ht, ', i-1, ', ', j-1,
-					', param[', SOMETHING_WITH_PARAMNUMS, ']);\n', sep='')
+					', param[', params[k]-1, ']);\n', sep='')
+				k <- k+1
 			}
 		}
 	}
@@ -23,5 +42,16 @@ dynr.loadings <- function(map, paramNums){
 	cat(ret)
 }
 
-#dynr.loadings( list(eta1=paste0('y', 1:4)) )
+# Examples
+# Single factor model with one latent variable
+#dynr.loadings( list(eta1=paste0('y', 1:4)), 4:6)
+
+# Two factor model with simple structure
+#dynr.loadings( list(eta1=paste0('y', 1:4), eta2=paste0('y', 5:7)), c(4:6, 1:2))
+
+# Two factor model with repeated use of a free parameter
+#dynr.loadings( list(eta1=paste0('y', 1:4), eta2=paste0('y', 5:8)), c(4:6, 1:2, 4))
+
+# Two factor model with a cross loading
+#dynr.loadings( list(eta1=paste0('y', 1:4), eta2=c('y5', 'y2', 'y6')), c(4:6, 1:2))
 
