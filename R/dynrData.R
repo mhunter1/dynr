f046a8f0ad38c4082c938741387dc83f2821a581
symiin <symiin@psu.edu>
2016-02-04 10:40:36 -0500

    Change dynr.Data to accommodate models with no covariate

--- R/dynrData.R
+++ R/dynrData.R
@@ -25,12 +25,14 @@
 #	displayDynrData(x) 
 #})
 
-dynr.data <- function(dataframe, id, time, observed, covariates){
+dynr.data <- function(dataframe, id, time, observed, covariates=NULL){
   ids <- unique(dataframe[,id])
   tstart <- c(sapply(1:length(ids),function(i){min(which(dataframe[,id]%in%ids[i]))})-1,dim(dataframe)[1])
   data.object <- list(id=dataframe[,id],tstart=tstart,time=dataframe[,time],observed=data.frame(dataframe[,observed]),covariates=data.frame(dataframe[,covariates]))
   names(data.object$observed) <- paste0("obs",1:length(observed))
-  names(data.object$covariates) <- paste0("covar",1:length(covariates))
+  if (!is.null(covariates)){
+    names(data.object$covariates) <- paste0("covar",1:length(covariates))
+  }else{names(data.object$covariates) <- NULL}
   return(data.object)
 }
 
