af25ba46cc914afb1209167c46ad4ba8eb05103f
mhunter1 <mhunter@ou.edu>
2016-03-18 01:06:49 -0500

    Add argument names to recipes in linear example

--- demo/LinearSDE.R
+++ demo/LinearSDE.R
@@ -11,9 +11,13 @@
 require(dynr)
 require(KFAS)
 
-meas <- dynr.matrixLoadings(values=matrix(c(1,0), 1, 2), params=matrix(0, 1, 2))
+meas <- dynr.matrixLoadings(
+	values=matrix(c(1,0), 1, 2),
+	params=matrix(0, 1, 2))
 
-ecov <- dynr.matrixErrorCov(diag(c(0.00001,1)), diag(c(0, 3)), diag(1.5,1), diag(4, 1))
+ecov <- dynr.matrixErrorCov(
+	values.latent=diag(c(0.00001,1)), params.latent=diag(c(0, 3)),
+	values.observed=diag(1.5,1), params.observed=diag(4, 1))
 ecov$c.string
 ecov$startval
 
@@ -21,6 +25,9 @@ initial<-dynr.initial(c(0,1), c(5,0), diag(1,2), diag(0,2))
 writeLines(initial$c.string)
 initial$startval
 
-dynamics <- dynr.linearDynamics(matrix(c(0, 1, 0, 2), 2, 2), matrix(c(0, 1, 1, 1), 2, 2), time="contin")
+dynamics <- dynr.linearDynamics(
+	params.dyn=matrix(c(0, 1, 0, 2), 2, 2),
+	values.dyn=matrix(c(0, 1, 1, 1), 2, 2),
+	time="contin")
 
 dP_dt <- dynr.dP_dt
