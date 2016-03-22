a3d439e7bb045f194ba089e157444ada9c2300a1
mhunter1 <mhunter@ou.edu>
2016-03-22 14:23:58 -0500

    Use cat instead of writeLines. Add empty transform function.

--- R/dynrCook.R
+++ R/dynrCook.R
@@ -9,12 +9,14 @@
 
 .softmaxCFunction <- "/**\n * This function takes a gsl_vector and modifies its second argument (another gsl_vector)\n * It computes the softmax function (e.g. for multinomial logistic regression)\n * @param x, vector of double values e.g. a vector of normally distributed numbers\n * @param result, softmax(x), e.g. a vector of numbers between 0 and 1 that sum to 1\n */\nvoid mathfunction_softmax(const gsl_vector *x, gsl_vector *result){\n\t/* Elementwise exponentiation */\n\tsize_t index=0;\n\tfor(index=0; index < x->size; index++){\n\t\tgsl_vector_set(result, index, exp(gsl_vector_get(x, index)));\n\t}\n\t\n\t/* Sum for the scaling coeficient */\n\tdouble scale = 0.0;\n\tfor(index=0; index < x->size; index++){\n\t\tscale += gsl_vector_get(result, index);\n\t}\n\t\n\t/* Multiply all elements of result by 1/scale */\n\tgsl_blas_dscal(1/scale, result);\n}\n"
 
-.cfunctions <- paste(.logisticCFunction, .softmaxCFunction, sep="\n")
+.transformCFunction <- "/**\n * This function modifies some of the parameters so that it satisfies the model constraint.\n * Do not include parameters in noise_cov matrices \n */\nvoid function_transform(double *param){\n}\n"
+
+.cfunctions <- paste(.logisticCFunction, .softmaxCFunction, .transformCFunction, sep="\n")
 
 dynr.cook <- function(file=stdout(), ...){
 	includes <- "#include <math.h>\n#include <gsl/gsl_matrix.h>\n#include <gsl/gsl_blas.h>\n"
 	glom <- paste(includes, paste(c(...), collapse="\n\n"), dynr.dP_dt, .cfunctions, sep="\n\n")
-	writeLines(text=glom, con=file)
+	cat(glom, file=file)
 }
 
 
