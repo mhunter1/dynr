3dfdbf2816557b437eee6e238c8a9c8fa1c6cee6
luou1 <lzo114@psu.edu>
2016-04-01 12:44:46 -0400

    started to add S4 classes for models

---
+++ R/dynrClass.R
@@ -0,0 +1,34 @@
+# 
+# dynr model CLASS
+# 
+
+setClass("dynrOptOptions",
+         representation(maxtime="numeric", 
+                        maxeval="integer",
+                        ftol_rel="numeric",
+                        xtol_rel="numeric",
+         )
+)
+setClass("dynrModel",
+         representation(response="list", # response models
+                        prior="ANY", # the prior model (multinomial)
+                        dens="array", # response densities (B)
+                        init="array", # usually called pi 
+                        nstates="numeric",
+                        nresp="numeric",
+                        ntimes="numeric",
+                        npars="numeric" # number of parameters
+                        num_regime="integer",
+                        dim_latent_var="integer",
+                        xstart="vector",
+                        ub="vector",
+                        lb="vector",
+                        options="list",
+                        isContinuousTime="logical",
+                        infile="character", 
+                        outfile="character", 
+                        verbose="logical",
+                        compileLib="logical"
+         )
+)
+
