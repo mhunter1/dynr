ffea835a85f2fc8a22f6cc5d46cc2749a2fa85fd
luou1 <lzo114@psu.edu>
2016-04-08 09:43:20 -0400

    Continuing restructure the model class

--- R/dynrClass.R
+++ R/dynrClass.R
@@ -1,7 +1,6 @@
 # 
 # dynr model CLASS
 # 
-
 setClass("dynrOptOptions",
          representation(maxtime="numeric", 
                         maxeval="integer",
@@ -9,23 +8,30 @@ setClass("dynrOptOptions",
                         xtol_rel="numeric"
          )
 )
+setClass(Class =  "dynrControl",
+         representation = representation(
+           xstart="vector",
+           ub="vector",
+           lb="vector",
+           options="dynrOptOptions",
+           isContinuousTime="logical",
+           infile="character",
+           outfile="character",
+           verbose="logical",
+           compileLib="logical",
+         ),
+         contains="dynrOptOptions"
+)
 setClass(Class =  "dynrModel",
          representation = representation(
-                     	num_regime="integer",
+                     	  num_regime="integer",
                         dim_latent_var="integer",
-                        xstart="vector",
-                        ub="vector",
-                        lb="vector",
-                        options="list",
-                        isContinuousTime="logical",
-                        infile="character",
-                        outfile="character",
-                        verbose="logical",
-                        compileLib="logical",
-			            dynamics =  "dynrDynamics",
-			            measurement = "dynrMeasurement",
-			            initial = "dynrInitial",
-						regimes= "dynrRegimes",
-						Class = "dynrNoise"
-         )
+                        opt.control="dynrControl",
+                        dynamics =  "dynrDynamics",
+			                  measurement = "dynrMeasurement",
+			                  initial = "dynrInitial",
+						            regimes= "dynrRegimes",
+						            noise = "dynrNoise"
+         ),
+         contains="dynrControl"
 )
