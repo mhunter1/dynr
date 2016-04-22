c755f2d135a8c2c19ccaa79be8f9c849356d6520
mhunter1 <mhunter@ou.edu>
2016-04-22 18:19:32 -0500

    Clean up data handling.  Bug fix for 1 covariate.

--- R/dynrData.R
+++ R/dynrData.R
@@ -26,17 +26,26 @@
 #})
 
 dynr.data <- function(dataframe, id, time, observed, covariates){
-  ids <- unique(dataframe[,id])
-  tstart <- c(sapply(1:length(ids),function(i){min(which(dataframe[,id]%in%ids[i]))})-1,dim(dataframe)[1])
-   if (!missing(covariates)){
-     data.object <- list(id=dataframe[,id],tstart=as.integer(tstart),time=as.double(dataframe[,time]),observed=data.frame(apply(dataframe[ , observed, drop=FALSE],2,as.double)),covariates=data.frame(apply(dataframe[,covariates],2,as.double)))
-    names(data.object$covariates) <- paste0("covar",1:length(covariates))
-   }
-  else{
-     data.object <- list(id=dataframe[,id],tstart=as.integer(tstart),time=as.double(dataframe[,time]),observed=data.frame(apply(dataframe[ , observed, drop=FALSE],2,as.double)))
-     }
-  names(data.object$observed) <- paste0("obs",1:length(observed))
-    return(data.object)
+	ids <- unique(dataframe[ , id])
+	tstart <- c(
+		sapply(1:length(ids),
+			function(i){
+				min(which(dataframe[,id] %in% ids[i]))
+			}
+		)-1,
+		dim(dataframe)[1])
+	data.object <- list(
+		id=dataframe[,id],
+		tstart=as.integer(tstart),
+		time=as.double(dataframe[,time]),
+		observed=data.frame(apply(dataframe[ , observed, drop=FALSE], 2, as.double))
+		)
+	if(!missing(covariates)){
+		data.object$covariates <- data.frame(apply(dataframe[,covariates, drop=FALSE], 2, as.double))
+		names(data.object$covariates) <- paste0("covar", 1:length(covariates))
+	}
+	names(data.object$observed) <- paste0("obs", 1:length(observed))
+	return(data.object)
 }
 
 
