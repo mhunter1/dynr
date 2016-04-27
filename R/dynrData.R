29706e7035a719311a2bad1d7f2a853c65bb5913
luou1 <lzo114@psu.edu>
2016-04-26 23:43:21 -0400

    added covariate names in the output list of dynr.data(); added documentations for dynr.data()

--- R/dynrData.R
+++ R/dynrData.R
@@ -25,6 +25,25 @@
 #	displayDynrData(x) 
 #})
 
+##' Create a list of data for parameter estimation (cooking dynr) using \code{\link{dynr.cook}}
+##'
+##' @param dataframe the data in the format of a data frame in R that contain a column of subject ID numbers 
+##' (i.e., an ID variable), a column indicating subject-specific measurement occasions
+##' (i.e., a TIME variable), at least one column of observed values, and any number of covariates. 
+##' If the data are fit to a discrete-time model, the TIME variable should contain subject-specific sequences 
+##' of consecutively equally spaced numbers (e.g, 1, 2, 3, ...).
+##' If the data are fit to a continuous-time model, the TIME varibles can contain subject-specific increasing sequences 
+##' of irregularly spaced real numbers.  
+##' Missing values in the observed variables shoud be indicated by NA. Missing values in covariates are not allowed. 
+##' That is, missing values in the covariates, if there are any, should be imputed first. 
+##' @param id a character string of the name of the ID variable in the data.
+##' @param time a character string of the name of the TIME variable in the data.
+##' @param observed a vector of character strings of the names of the observed variables in the data.
+##' @param covariates a vector of character strings of the names of the covariates in the data, which can be missing.
+##' 
+##' @examples
+##' data <- dynr.data(thedata, id="ID", time="Time",observed=paste0('y', 1:6), 
+##' covariates=paste0('x', 1:2))
 dynr.data <- function(dataframe, id, time, observed, covariates){
 	ids <- unique(dataframe[ , id])
 	tstart <- c(
@@ -41,6 +60,7 @@ dynr.data <- function(dataframe, id, time, observed, covariates){
 		observed=data.frame(apply(dataframe[ , observed, drop=FALSE], 2, as.double))
 		)
 	if(!missing(covariates)){
+	  data.object$covariate.names <- covariates
 		data.object$covariates <- data.frame(apply(dataframe[,covariates, drop=FALSE], 2, as.double))
 		names(data.object$covariates) <- paste0("covar", 1:length(covariates))
 	}
