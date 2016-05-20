72f375ef78f4b36dd34a088c395bd78edb11b988
luou1 <lzo114@psu.edu>
2016-05-20 12:53:03 +0800

    added package source to sourced functions

--- R/dynrPlot.R
+++ R/dynrPlot.R
@@ -22,15 +22,15 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     
   } else {
     # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    grid::grid.newpage()
+    grid::pushViewport(grid::viewport(layout = grid::grid.layout(nrow(layout), ncol(layout))))
     
     # Make each plot, in the correct location
     for (i in 1:numPlots) {
       # Get the i,j matrix positions of the regions that contain this subplot
       matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
       
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+      print(plots[[i]], vp = grid::viewport(layout.pos.row = matchidx$row,
                                       layout.pos.col = matchidx$col))
     }
   }
@@ -122,14 +122,14 @@ p1 <- dynr.ggplot(res, data.dynr=data.dynr,numSubjDemo=1,...)
 
 if (model$num_regime>1){
   highProbR <- data.frame(regime=apply(res@pr_t_given_T,2,which.max))
-  p2 <- ggplot(data = highProbR, aes(factor(regime))) +
+  p2 <- ggplot2::ggplot(data = highProbR, ggplot2::aes(factor(regime))) +
   geom_bar() +
-  scale_fill_brewer(palette = 3) +
-  xlab('Regime') + ylab('Counts') + labs(fill = '') + 
+  ggplot2::scale_fill_brewer(palette = 3) +
+  xlab('Regime') + ylab('Counts') + ggplot2::labs(fill = '') + 
   ggtitle('Counts by most probable regime') +
-  theme(axis.title = element_text(size=14),
-        axis.text=element_text(size=12),
-        plot.title = element_text(size = 12, 
+  ggplot2::theme(axis.title = ggplot2::element_text(size=14),
+        axis.text=ggplot2::element_text(size=12),
+        plot.title = ggplot2::element_text(size = 12, 
                                   colour = "black", face = "bold"))
 }
 
@@ -174,11 +174,11 @@ plotFormula <- function(object,model,
   minx <- 1; maxx <- 10
   miny <- 1; maxy <- 50
   df <- data.frame(x=minx:maxx,y=miny:maxy)
-  p3 <- ggplot2::ggplot(df) + ggplot2::theme_void() +xlim(minx,maxx)+ylim(miny,maxy)
+  p3 <- ggplot2::ggplot(df) + ggplot2::theme_void() +ggplot2::xlim(minx,maxx)+ggplot2::ylim(miny,maxy)
   maxy2 <- maxy
   
   if (toPlot=="dyn" || toPlot=="both"){
-  p3 <- p3 + annotate(geom="text", x=(maxx-minx)/4, 
+  p3 <- p3 + ggplot2::annotate(geom="text", x=(maxx-minx)/4, 
              y=c(maxy-(0:(nregime_dyn-1))*(ne_dyn*spacing)), 
              label=a_dyn,
              color="black",size=textsize,hjust=.2,...)
@@ -188,7 +188,7 @@ plotFormula <- function(object,model,
     for (k in 1:ne_dyn){
     #i <- k+(j-1)*ne
     #rnow <- ceiling(i/ne)
-    p3<-p3+annotate(geom="text", x=(maxx-minx)/4, 
+    p3<-p3+ggplot2::annotate(geom="text", x=(maxx-minx)/4, 
                     #y=qq[rnow]-6*(i-ifelse(rnow>1,rnow,0)),
                     y=qq[j] - k*((abs(diff(qq)[j])/ne_dyn)/3),
                     label=as.character(formula_dyn[[j]][[k]]),
@@ -198,7 +198,7 @@ plotFormula <- function(object,model,
   }#End of toPlot == dyn or both
   
   if (toPlot=="meas" || toPlot=="both"){
-    p3 <- p3 + annotate(geom="text", x=(maxx-minx)/4, 
+    p3 <- p3 + ggplot2::annotate(geom="text", x=(maxx-minx)/4, 
                y=c(maxy2-(0:(nregime_obs-1))*(ne_obs*spacing)), 
                label=a_obs,
                color="black",size=textsize,hjust=.2,...)
@@ -209,7 +209,7 @@ plotFormula <- function(object,model,
       for (k in 1:ne_obs){
         #i <- k+(j-1)*ne_obs
         #rnow <- ceiling(i/ne)
-        p3<-p3+annotate(geom="text", x=(maxx-minx)/4, 
+        p3<-p3+ggplot2::annotate(geom="text", x=(maxx-minx)/4, 
                         #y=qq[rnow]-6*(i-ifelse(rnow>1,rnow,0)),
                         y=qq[j] - k*((abs(diff(qq)[j])/ne_obs)/2),
                         label=as.character(formula_obs[[j]][[k]]),
@@ -220,7 +220,7 @@ plotFormula <- function(object,model,
 }
 
 
-##' The ggplot of the smoothed state estimates and the most likely regimes
+##' The ggplot2::ggplot of the smoothed state estimates and the most likely regimes
 ##' 
 ##' @param res The dynr object returned by dynr.cook().
 ##' @param data.dynr The dynr data returned by dynr.data().
@@ -236,7 +236,7 @@ plotFormula <- function(object,model,
 ##' @param fillPalette A palette function for blocks that when called with a single integer argument (the number of levels in the scale) returns the values that they should take.
 ##' @param mancolorPalette A color palette for manually scaling the colors of lines and plots.
 ##' @param manfillPalette A color palette for manually scaling the colors of filled blocks.
-##' @param ... A list of element name, element pairings that modify the existing ggplot theme. Consult the theme() function in the R package ggplot.
+##' @param ... A list of element name, element pairings that modify the existing ggplot2::ggplot ggplot2::theme. Consult the ggplot2::theme() function in the R package ggplot2::ggplot.
 dynr.ggplot <- function(res, data.dynr, numSubjDemo=2, 
                         states, names.state, 
                         names.regime,shape.values,
@@ -277,60 +277,60 @@ dynr.ggplot <- function(res, data.dynr, numSubjDemo=2,
 		data.plot <- cbind(data.plot, matrix(res@eta_smooth_final[states,],ncol=length(states),byrow=TRUE))
 		names(data.plot) <- c(names.id.vars, names.measure.vars)
 		
-		data_long<- melt(data.plot[data.plot$id%in%randid,],
+		data_long<- reshape2::melt(data.plot[data.plot$id%in%randid,],
 			id.vars=names.id.vars,
 			measure.vars=names.measure.vars,
 			value.name="value")
     
-		partial.plot<-ggplot(data_long, aes(x=time, y=value, colour = variable)) + 
-		  geom_line(size=1) +
-		  geom_point(size=4, aes(shape=variable)) +
-		  scale_shape_manual(values=shape.values)+
-		  facet_wrap(~id)+
-		  labs(title = title,y=ylab,ggtitle="")+
-		  theme(plot.title=element_text(lineheight=.8, face="bold"), legend.position="bottom",legend.text=element_text(lineheight=0.8, face="bold"),...)
+		partial.plot<-ggplot2::ggplot(data_long, ggplot2::aes(x=time, y=value, colour = variable)) + 
+		  ggplot2::geom_line(size=1) +
+		  ggplot2::geom_point(size=4, ggplot2::aes(shape=variable)) +
+		  ggplot2::scale_shape_manual(values=shape.values)+
+		  ggplot2::facet_wrap(~id)+
+		  ggplot2::labs(title = title,y=ylab,ggtitle="")+
+		  ggplot2::theme(plot.title=ggplot2::element_text(lineheight=.8, face="bold"), legend.position="bottom",legend.text=ggplot2::element_text(lineheight=0.8, face="bold"),...)
 
 	}else{
 		
-		data.plot <- ddply(data.plot,"id",addendtime)
+		data.plot <- plyr::ddply(data.plot,"id",addendtime)
 		data.plot$regime<-as.factor(apply(res@pr_t_given_T,2,findRegime))
 		names.id.vars<-c("id","time","endtime","regime")
 		names.measure.vars<-names.state
 		data.plot<-cbind(data.plot,matrix((res@eta_smooth_final[states,]),ncol=length(states),byrow=TRUE))
 		names(data.plot)<-c(names.id.vars,names.measure.vars)
 		
-		data_long<- melt(data.plot[data.plot$id%in%randid,],
+		data_long<- reshape2::melt(data.plot[data.plot$id%in%randid,],
 			id.vars=names.id.vars,
 			measure.vars=names.measure.vars,
 			value.name="value")
     #data_long$statenumber<-as.factor(sub("state","",data_long$variable))
-    partial.plot<-ggplot(data_long,aes(x=time, y=value, group=variable)) +
-		  geom_rect(aes(xmin=time, xmax=endtime, ymin=-Inf, ymax=Inf, fill=regime), alpha=.15) +
-		  geom_line(size=1, aes(color=variable)) +
-      geom_point(size=4, aes(color=variable,shape=variable)) +
-      scale_shape_manual(values=shape.values)+
-		  #geom_text(size=1, aes(label=statenumber,color=variable))+
-		  facet_wrap(~id)+ 
-		  labs(title = title,y=ylab,ggtitle="")+
-		  theme(plot.title=element_text(lineheight=.8, face="bold"), legend.position="bottom",legend.text=element_text(lineheight=0.8, face="bold"),...)
+    partial.plot<-ggplot2::ggplot(data_long,ggplot2::aes(x=time, y=value, group=variable)) +
+		  ggplot2::geom_rect(ggplot2::aes(xmin=time, xmax=endtime, ymin=-Inf, ymax=Inf, fill=regime), alpha=.15) +
+		  ggplot2::geom_line(size=1, ggplot2::aes(color=variable)) +
+      ggplot2::geom_point(size=4, ggplot2::aes(color=variable,shape=variable)) +
+      ggplot2::scale_shape_manual(values=shape.values)+
+		  #geom_text(size=1, ggplot2::aes(label=statenumber,color=variable))+
+		  ggplot2::facet_wrap(~id)+ 
+		  ggplot2::labs(title = title,y=ylab,ggtitle="")+
+		  ggplot2::theme(plot.title=ggplot2::element_text(lineheight=.8, face="bold"), legend.position="bottom",legend.text=ggplot2::element_text(lineheight=0.8, face="bold"),...)
 	
 		}
   
 	if(is.bw){
 	  bw.plot<-partial.plot+
-	    scale_fill_grey(start = 0.1, end = 0.9)+
-	    scale_color_grey(start = 0.2, end = 0.7)
+	    ggplot2::scale_fill_grey(start = 0.1, end = 0.9)+
+	    ggplot2::scale_color_grey(start = 0.2, end = 0.7)
 	  print(bw.plot)
 	}else{
 	  default.plot<-partial.plot+
-	    scale_colour_brewer(palette=colorPalette)+
-	    scale_fill_brewer(palette=fillPalette)		  
+	    ggplot2::scale_colour_brewer(palette=colorPalette)+
+	    ggplot2::scale_fill_brewer(palette=fillPalette)		  
 	  if(!missing(mancolorPalette)){
-	    manual.plot<-partial.plot+scale_colour_manual(values=mancolorPalette)
+	    manual.plot<-partial.plot+ggplot2::scale_colour_manual(values=mancolorPalette)
 	  }
 	  if(!missing(manfillPalette)){
 	    if(!exists("manual.plot")){manual.plot<-partial.plot}
-	    manual.plot<-manual.plot+scale_fill_manual(values=manfillPalette)
+	    manual.plot<-manual.plot+ggplot2::scale_fill_manual(values=manfillPalette)
 	  }
 	  if (exists("manual.plot")){
 	    return(manual.plot) #print(manual.plot)
