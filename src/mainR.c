77254ab59dc3e5a8d7b0c67187dd98580bfa083e
mhunter <mhunter@ou.edu>
2017-03-10 15:46:42 -0600

    Initialize initial conditions for first data row

--- src/mainR.c
+++ src/mainR.c
@@ -512,7 +512,7 @@ SEXP main_R(SEXP model_list, SEXP data_list, SEXP weight_flag_in, SEXP debug_fla
 	    }
 
 
-	    data_model.pc.func_initial_condition(par.func_param, data_model.co_variate, pi.pr_0, pi.eta_0, pi.error_cov_0);
+	    data_model.pc.func_initial_condition(par.func_param, data_model.co_variate[0], pi.pr_0, pi.eta_0, pi.error_cov_0);
 
 	    par.eta_noise_cov=gsl_matrix_calloc(data_model.pc.dim_latent_var, data_model.pc.dim_latent_var);
 	    par.y_noise_cov=gsl_matrix_calloc(data_model.pc.dim_obs_var, data_model.pc.dim_obs_var);
