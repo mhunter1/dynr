e282ebc454a9ff6607b07cec2f02b5d7fee6e9f0
mhunter <mhunter@ou.edu>
2017-05-04 13:11:47 -0500

    Update draft C code for initial covariates

--- demo/initialExo.R
+++ demo/initialExo.R
@@ -50,12 +50,13 @@ initial <- prep.initial(
 #/******************C Code generated********************/#
 #void function_initial_condition(double *param, gsl_vector **co_variate, gsl_vector *pr_0, gsl_vector **eta_0, gsl_matrix **error_cov_0, size_t *index_sbj){
 #	
-#	gsl_vector *covariate_local = gsl_vector_calloc(1);
-#	
 #	gsl_vector *Pvector = gsl_vector_calloc(1);
 #	gsl_vector_set(Pvector, 0, 1);
 #	mathfunction_softmax(Pvector, pr_0);
 #	
+#	gsl_vector *eta_local = gsl_vector_calloc(2);
+#	gsl_vector *covariate_local = gsl_vector_calloc(1);
+#	gsl_matrix *CMatrix = gsl_matrix_calloc(2, 1);
 #	size_t num_regime=pr_0->size;
 #	size_t dim_latent_var=error_cov_0[0]->size1;
 #	size_t num_sbj=(eta_0[0]->size)/(dim_latent_var);
@@ -63,10 +64,20 @@ initial <- prep.initial(
 #	size_t regime;
 #	for(regime=0; regime < num_regime; regime++){
 #		for(i=0; i < num_sbj; i++){
+#			gsl_vector_set(eta_local, 0, param[4]);
+#			gsl_vector_set(eta_local, 1, 1);
+#			gsl_matrix_set(CMatrix, 0, 0, param[5]);
+#			gsl_blas_dgemv(CblasNoTrans, 1.0, CMatrix, covariate_local, 1.0, eta_local);
 #			gsl_vector_set((eta_0)[regime], i*dim_latent_var+0, param[4]);
 #			gsl_vector_set((eta_0)[regime], i*dim_latent_var+1, 1);
+#			gsl_vector_set((eta_0)[regime], i*dim_latent_var+2, param[5]);
+#			gsl_vector_set_zero(eta_local);
+#			gsl_matrix_set_zero(CMatrix);
 #		}
 #	}
 #	gsl_vector_free(Pvector);
+#	gsl_vector_free(eta_local);
 #	gsl_vector_free(covariate_local);
+#	gsl_matrix_free(CMatrix);
 #}
+
