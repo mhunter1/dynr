mhunter1/dynr

commit 0f50c5957f55df9eb965e94883044d99585c2a3b

Change small value to powers of 1e-10

src/brekfis.c
@@ -271,7 +271,7 @@ double brekfis(gsl_vector ** y, gsl_vector **co_variate, size_t total_time, doub
 					
 					/** compare the p with the (0.0001) and get the bigger one. We do not like probability that is too small. :)**/
 					double numNotMissingVars = mathfunction_sum_vector(y_non_miss);
-					double tooSmallNumber = numNotMissingVars < 80 ? pow(1e-4, numNotMissingVars):1e-322;
+					double tooSmallNumber = numNotMissingVars < 32 ? pow(1e-10, numNotMissingVars):1e-322;
 					double tryP = exp(-neg_log_p);
 					p = ( isfinite(tryP) && (tryP > tooSmallNumber) ) ? tryP:tooSmallNumber;
 					

commit 7ca78b073cc7212efc1ba08c37e38b0f3e00c240

Change 'too small' criteria to vary by powers of 1e-4
As indicated by Sy-Miin's testing, commit 0f50c59 broke
a Monte Carlo simulation.  This may fix it.

src/brekfis.c
@@ -276,7 +276,7 @@ double brekfis(gsl_vector ** y, gsl_vector **co_variate, size_t total_time, doub
 					
 					/** compare the p with the (0.0001) and get the bigger one. We do not like probability that is too small. :)**/
 					double numNotMissingVars = mathfunction_sum_vector(y_non_miss);
-					double tooSmallNumber = numNotMissingVars < 30 ? pow(1e-10, numNotMissingVars):1e-300;
+					double tooSmallNumber = numNotMissingVars < 75 ? pow(1e-4, numNotMissingVars):1e-300;
 					double tryP = exp(-neg_log_p);
 					p = ( isfinite(tryP) && (tryP > tooSmallNumber) ) ? tryP:tooSmallNumber;
 					
@@ -330,7 +330,7 @@ double brekfis(gsl_vector ** y, gsl_vector **co_variate, size_t total_time, doub
             	}/*end of k*/
 
             /** step 2.4.1: check whether there is zero probability. If so, a small amount of value is added. Again we do not like too small and zero probability **/
-			double tooSmallRegimeNumber = config->num_regime < 30 ? pow(1e-10, config->num_regime):1e-300;
+			double tooSmallRegimeNumber = config->num_regime < 75 ? pow(1e-4, config->num_regime):1e-300;
 			if(gsl_vector_min(pr_t) < tooSmallRegimeNumber){
 				gsl_vector_add_constant(pr_t, tooSmallRegimeNumber);
 				mathfunction_vector_normalize(pr_t);
@@ -963,7 +963,7 @@ double EKimFilter(gsl_vector ** y, gsl_vector **co_variate, double *y_time, cons
                    neg_log_p=mathfunction_negloglike_multivariate_normal_invcov(innov_v[t][regime_j][regime_k], inv_residual_cov[t][regime_j][regime_k], y_non_miss, innov_determinant);
 
 					double numNotMissingVars = mathfunction_sum_vector(y_non_miss);
-					double tooSmallNumber = numNotMissingVars < 30 ? pow(1e-10, numNotMissingVars):1e-300;
+					double tooSmallNumber = numNotMissingVars < 75 ? pow(1e-4, numNotMissingVars):1e-300;
 					double tryP = exp(-neg_log_p);
 					p = ( isfinite(tryP) && (tryP > tooSmallNumber) ) ? tryP:tooSmallNumber;
 
@@ -1007,7 +1007,7 @@ double EKimFilter(gsl_vector ** y, gsl_vector **co_variate, double *y_time, cons
             }/*end of k*/
 			
             /** step 2.4.1: check whether there is zero probability. If so, a small amount of value is added. Again we do not like too small and zero probability **/
-			double tooSmallRegimeNumber = config->num_regime < 30 ? pow(1e-10, config->num_regime):1e-300;
+			double tooSmallRegimeNumber = config->num_regime < 75 ? pow(1e-4, config->num_regime):1e-300;
 	    	
 			if(gsl_vector_min(pr_t[t]) < tooSmallRegimeNumber){
 	        	gsl_vector_add_constant(pr_t[t], tooSmallRegimeNumber);