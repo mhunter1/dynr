b51bb09de62f18ebd91dea6364faa8a25d456aeb
Dongjun You <dongjuny2@gmail.com>
2018-10-31 13:06:07 -0400

    Trim and arrange the demo file of outlier detection for linear models

--- demo/OutlierDetection.R
+++ demo/OutlierDetection.R
@@ -60,42 +60,30 @@ init_shk <- prep.initial(
   params.inicov=matrix(c('c_11','c_12',
                          'c_12','c_22'), ncol=2, byrow=TRUE) )
 
-# define the differential equation
+# define the transition matrix
 dynm_shk <- prep.matrixDynamics(
-  values.dyn=matrix(c(0.8, -0.2,
-                      -0.2,  0.7), ncol=2, byrow=TRUE),
+  values.dyn=matrix(c(0.6, -0.3,
+                      -0.2,  0.5), ncol=2, byrow=TRUE),
   params.dyn=matrix(c('b_11','b_12',
                       'b_21','b_22'), ncol=2, byrow=TRUE),
   isContinuousTime=FALSE)
 
-formula_shk <- list(list(eta_1 ~ b_11*eta_1 - b_12*eta_2,
-                         eta_2 ~ b_21*eta_1 + b_22*eta_2))
-dynm_shk2 <- prep.formulaDynamics(formula=formula_shk,
-                                  startval=c(b_11=.6, b_12=-.2, b_21=-.2, b_22=.6),
-                                 isContinuousTime=FALSE)
 # Prepare for cooking
 # put all the recipes together
 model_shk <- dynr.model(dynamics=dynm_shk, measurement=meas_shk,
                         noise=nois_shk, initial=init_shk,
                         data=data_shk, outfile=paste0("model_shk.c"))
-model_shk2 <- dynr.model(dynamics=dynm_shk2, measurement=meas_shk,
-                         noise=nois_shk, initial=init_shk,
-                         data=data_shk, outfile=paste0("model_shk2.c"))
 
 # Estimate free parameters
-# SHOULD cook with 'debug_flag=TRUE'
+# SHOULD cook with 'debug_flag=TRUE' to extract information 
+#   used for outlier detection
 cook_shk <- dynr.cook(model_shk, debug_flag=TRUE)
-cook_shk2 <- dynr.cook(model_shk2, debug_flag=TRUE)
-cocook <- coef(cook_shk2)
-cocook[model_shk2$dynamics@paramnames]
-plotFormula(model_shk2, cook_shk2@transformed.parameters)
 
 #------------------------------------------------------------
 # detect outliers
 # output of 'dynr.taste' is a list containing lists of results from the outlier detection process,
 #   which will be input of the autoplot.dynrTaste and dynr.taste2
 taste_shk <- dynr.taste(model_shk, cook_shk, conf.level=.99)
-taste_shk2 <- dynr.taste(model_shk2, cook_shk2, dynm_shk2, conf.level=.99)
 
 ## create ggplot objects
 # for 2 participants (default) who have largest chi-square values,
@@ -118,13 +106,14 @@ taste_plot3
 # apply the detected outliers, and re-fit the model
 taste2_shk <- dynr.taste2(model_shk, cook_shk, taste_shk,
                           newOutfile="taste2_shk.c")
-taste2_shk2 <- dynr.taste2(model_shk2, cook_shk2, taste_shk2,
-                          newOutfile="taste2_shk2.c")
 
 # compare true parameter and estimated ones
 #  cook: parameter estimates when the data include true outliers
 #  taste2: parameter estimates after applying the estimated outliers by the function 'dynr.taste2'
-# check the how closely recover the true parameter from 'dynr.taste2', i.e., after applying outliers
-data.frame(true=true_params, cook=coef(cook_shk)[1:17],
+# check the how closely recover the true parameter
+# from 'dynr.taste2', i.e., after applying outliers
+data.frame(true=true_params, 
+           cook=coef(cook_shk)[1:17],
            taste2=coef(taste2_shk$dynrCook_new)[1:17])
+
 
