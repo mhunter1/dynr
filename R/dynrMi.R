c8f25d8bce50b22a6fb3bedc47c5761c46846fa9
YanlingLi <yxl823@psu.edu>
2019-02-07 00:19:16 -0500

    add some annotations to dynr.mi

--- R/dynrMi.R
+++ R/dynrMi.R
@@ -31,13 +31,13 @@ dynr.mi <- function(model, aux.variable, m=5, iter, imp.obs=FALSE, imp.exo=FALSE
 	id <- unique(ID)   # number of subjects
 	time <- model$data$time
 	
-	# data from dynr.model
+	# raw data 
 	datanolag <- cbind(ID,y,x,au) 
 	
-	# variables to create lags on
+	# select variables to create lags on
 	dataforlag <- subset(datanolag,select = c("ID", lag.variable))  
 	
-	# create a null data frame to store lag variables 
+	# create a null data frame to store lagged variables 
 	datalag <- data.frame(ID, matrix(NA, nrow = length(ID), ncol = length(lag.variable)*lag))
 	
 	if(lag < 1){
@@ -46,6 +46,7 @@ dynr.mi <- function(model, aux.variable, m=5, iter, imp.obs=FALSE, imp.exo=FALSE
 	}
 	
 	for(i in id){
+	  # tmp used to store original variables for each subject 
 	  tmp <- dataforlag[dataforlag$ID == i,]
 	  tmp <- as.matrix(subset(tmp, select = -ID))
 	  P <- ncol(tmp)
@@ -58,13 +59,13 @@ dynr.mi <- function(model, aux.variable, m=5, iter, imp.obs=FALSE, imp.exo=FALSE
 	  for(t in 1:lag){
 	    if (leads == TRUE)  { tmp1[1:(nt-t),] <- tmp[(t+1):nt,] }
 	    else  { tmp1[(t+1):nt,] <- tmp[1:(nt-t),] }
-	    a <- P*(t-1)+2
-	    b <- P*t+1
+	    a <- P*(t-1)+2 
+	    b <- P*t+1 
 	    datalag[datalag$ID == i, ][, a:b] <- tmp1
 	  }
 	}
 
-	# combine original variables and lag variables 
+	# combine original variables and lagged variables 
 	dataformice <- data.frame(datanolag, subset(datalag, select = -ID))
 
 	
@@ -76,27 +77,32 @@ dynr.mi <- function(model, aux.variable, m=5, iter, imp.obs=FALSE, imp.exo=FALSE
 	  
 	  chains = m
 	  
-	  ###Reformat chainMean to a list
+	  # reformat imp$chainMean to a matrix called coda
 	  chainmean = imp$chainMean
 	  chainmean2 = chainmean[!is.na(chainmean)]
 	  coda = matrix(chainmean2, nrow = m*iter, byrow = T)
 	  
+	  # create a null matrix to store Rhats for all variables
 	  Rhat = matrix(NA, nrow = iter, ncol = nvariables) 
+	  
 	  #cal Rhats for each iteration and each variable
 	  for(i in itermin:iter){
 	    
+	    # select all values generated from i iterations
 	    codai = coda[1:m*i,1:nvariables]
 	    value = list()  #each list contains a chain 
 	    for(k in 1:m){
 	      value[[k]] = coda[(i*(k-1)+1+burn):(i*k),]  #drop the values of each chain in the beginning period
 	    }
 	    
-	    # RHAT calc from zcalc
+	    # cal Rhats
 	    iterations = i
 	    for(j in 1:nvariables) {
 	      
+	      # create vectors to store means and variations of values in chains for each variable
 	      chainmeans = rep(NA, chains)
 	      chainvars = rep(NA, chains)
+	      
 	      for(k in 1:chains) {
 	        sum = sum(value[[k]][,j])
 	        var = var(value[[k]][,j])
@@ -113,7 +119,7 @@ dynr.mi <- function(model, aux.variable, m=5, iter, imp.obs=FALSE, imp.exo=FALSE
 	      
 	      varplus = (iterations - 1) * globalvar / iterations + b / iterations
 	      
-	      # Gelman-Rubin statistic
+	      # Gelman-Rubin statistic (Rhat)
 	      rhat = sqrt(varplus / globalvar)
 	      Rhat[i,j] = rhat
 	    }
@@ -123,17 +129,17 @@ dynr.mi <- function(model, aux.variable, m=5, iter, imp.obs=FALSE, imp.exo=FALSE
 	
 	
 	if (diag == TRUE){
-	  # trace plots
-	  plot(imp) 
+	  # trace plots from mice()
+	  plot(imp, c(ynames,xnames)) 
 	  
-    # Rhat plot
+    # Rhat plots from diag.mi()
 	  nvariables = length(c(ynames,xnames))
 	  result = diag.mi(imp, nvariables, m, 2,iter,0)
 	  
 	  names =c(ynames,xnames)
 	  for(j in 1:nvariables){
 	    plot(2:iter, result[2:iter,j],type="l",ylim=c(min(na.omit(result)),max(na.omit(result))),ylab="Rhat",xlab="last iteration in chain",main=names[j])
-	    abline(h=1.1,lty=2,col=2)
+	    abline(h=1.1,lty=2,col=2)  # criterion set as Rhat < 1.1
 	  }
 	}
 	
@@ -176,6 +182,7 @@ dynr.mi <- function(model, aux.variable, m=5, iter, imp.obs=FALSE, imp.exo=FALSE
 		trial <- dynr.cook(modelnew, verbose = FALSE)  #names(trial) get names of the params
 		#summary(trial)
 		
+		# save dynr.cook results
 		if (cook.save == TRUE)
 		  save(trial, file=paste0("cookresult",j,".rdata"))
 		
