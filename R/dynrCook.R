6290ef57c15532e6e23594228d2ba8610d881f07
U-MAVIS\mhunter <mhunter.ou@gmail.com>
2019-03-04 16:10:21 -0500

    Document exit status -6

--- R/dynrCook.R
+++ R/dynrCook.R
@@ -445,7 +445,9 @@ confint.dynrCook <- function(object, parm, level = 0.95, type = c("delta.method"
 ##' OUT_OF_MEMORY \tab -3 \tab Ran out of memory. \cr
 ##' ROUNDOFF_LIMITED \tab -4 \tab Halted because roundoff errors limited progress. (In this case, the optimization still typically returns a useful result.) \cr
 ##' FORCED_STOP \tab -5 \tab Halted because of a forced termination: the user called nlopt_force_stop(opt) on the optimization's nlopt_opt object opt from the user's objective function or constraints. \cr
+##' NONFINITE_FIT \tab -6 \tab Fit function is not finite (i.e., is NA, NaN, Inf or -Inf). \cr
 ##' }
+##' The last row of this table corresponding to an exit code of -6, is not from NLOPT, but rather is specific to the \code{dynr} package.
 ##' 
 ##' @seealso 
 ##' \code{\link{autoplot}}, \code{\link{coef}}, \code{\link{confint}},
