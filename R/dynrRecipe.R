mhunter1/dynr

commit f604f72006b912e1e7f21f39a09035509309498f

Add row and column names to measurement from obs.names and state.names
Relates to issue #207

R/dynrRecipe.R
@@ -1530,18 +1530,18 @@ setMethod("diag", "character", #diag.character <-
 
 
 # allow free/fixed to be used in values/params arguments
-preProcessValues <- function(x){
+preProcessValues <- function(x, rowNam, colNam){
 	x[is.na(x)] <- 'freed'
 	if(is.null(dim(x))){
 		numRow <- length(x)
 		numCol <- 1
-		rowNam <- NULL
-		colNam <- NULL
+		if(missing(rowNam)) rowNam <- NULL
+		if(missing(colNam)) colNam <- NULL
 	} else {
 		numRow <- nrow(x)
 		numCol <- ncol(x)
-		rowNam <- rownames(x)
-		colNam <- colnames(x)
+		if(missing(rowNam)) rowNam <- rownames(x)
+		if(missing(colNam)) colNam <- colnames(x)
 	}
 	x <- c(x)
 	xl <- tolower(x)
@@ -1557,18 +1557,18 @@ preProcessValues <- function(x){
 	return(x)
 }
 
-preProcessParams <- function(x){
+preProcessParams <- function(x, rowNam, colNam){
 	if (!is.null(x)) {x[is.na(x)] <- 'fixed'}
 	if(is.null(dim(x))){
 		numRow <- length(x)
 		numCol <- 1
-		rowNam <- NULL
-		colNam <- NULL
+		if(missing(rowNam)) rowNam <- NULL
+		if(missing(colNam)) colNam <- NULL
 	} else {
 		numRow <- nrow(x)
 		numCol <- ncol(x)
-		rowNam <- rownames(x)
-		colNam <- colnames(x)
+		if(missing(rowNam)) rowNam <- rownames(x)
+		if(missing(colNam)) colNam <- colnames(x)
 	}
 	x <- c(x)
 	xl <- tolower(x)
@@ -1891,12 +1891,12 @@ prep.measurement <- function(values.load, params.load=NULL, values.exo=NULL, par
 		stop("'exo.names' uses some of the same names more than once.\n  You cannot be the King of Crimson with this indiscipline.")
 	}
 	
-	values.load <- lapply(values.load, preProcessValues)
-	params.load <- lapply(params.load, preProcessParams)
-	values.exo <- lapply(values.exo, preProcessValues)
-	params.exo <- lapply(params.exo, preProcessParams)
-	values.int <- lapply(values.int, preProcessValues)
-	params.int <- lapply(params.int, preProcessParams)
+	values.load <- lapply(values.load, preProcessValues, rowNam=obs.names, colNam=state.names)
+	params.load <- lapply(params.load, preProcessParams, rowNam=obs.names, colNam=state.names)
+	values.exo <- lapply(values.exo, preProcessValues, rowNam=obs.names, colNam=exo.names)
+	params.exo <- lapply(params.exo, preProcessParams, rowNam=obs.names, colNam=exo.names)
+	values.int <- lapply(values.int, preProcessValues, rowNam=obs.names, colNam='one')
+	params.int <- lapply(params.int, preProcessParams, rowNam=obs.names, colNam='one')
 	
 	values.load <- lapply(values.load, preProcessNames, obs.names, state.names, 'values.load', 'obs.names', 'state.names')
 	#params.load <- lapply(params.load, preProcessNames, obs.names, state.names, 'values.load', 'obs.names', 'state.names')

commit 7c885c1aeea9ecbb456ca95c2fecd59120d03de8

Move name-checking before matrix pre-processing
This commit f604f72006b912e1e7f21f39a09035509309498f broke the
test inst/models/passing/errorCheckMeas.R

R/dynrRecipe.R
@@ -1888,19 +1888,19 @@ prep.measurement <- function(values.load, params.load=NULL, values.exo=NULL, par
 		stop("'exo.names' uses some of the same names more than once.\n  You cannot be the King of Crimson with this indiscipline.")
 	}
 	
-	values.load <- lapply(values.load, preProcessValues, rowNam=obs.names, colNam=state.names)
-	params.load <- lapply(params.load, preProcessParams, rowNam=obs.names, colNam=state.names)
-	values.exo <- lapply(values.exo, preProcessValues, rowNam=obs.names, colNam=exo.names)
-	params.exo <- lapply(params.exo, preProcessParams, rowNam=obs.names, colNam=exo.names)
-	values.int <- lapply(values.int, preProcessValues, rowNam=obs.names, colNam='one')
-	params.int <- lapply(params.int, preProcessParams, rowNam=obs.names, colNam='one')
-	
 	values.load <- lapply(values.load, preProcessNames, obs.names, state.names, 'values.load', 'obs.names', 'state.names')
 	#params.load <- lapply(params.load, preProcessNames, obs.names, state.names, 'values.load', 'obs.names', 'state.names')
 	values.exo <- lapply(values.exo, preProcessNames, obs.names, exo.names, 'values.exo', 'obs.names', 'exo.names')
 	#params.exo <- lapply(params.exo, preProcessNames, obs.names, exo.names, 'values.exo', 'obs.names', 'exo.names')
 	values.int <- lapply(values.int, preProcessNames, obs.names, character(0), 'values.int', 'obs.names')
 	#params.int <- lapply(params.int, preProcessNames, obs.names, 'values.int', 'obs.names')
+	
+	values.load <- lapply(values.load, preProcessValues, rowNam=obs.names, colNam=state.names)
+	params.load <- lapply(params.load, preProcessParams, rowNam=obs.names, colNam=state.names)
+	values.exo <- lapply(values.exo, preProcessValues, rowNam=obs.names, colNam=exo.names)
+	params.exo <- lapply(params.exo, preProcessParams, rowNam=obs.names, colNam=exo.names)
+	values.int <- lapply(values.int, preProcessValues, rowNam=obs.names, colNam='one')
+	params.int <- lapply(params.int, preProcessParams, rowNam=obs.names, colNam='one')
 	
 	# Check that all 'values' imply 0, 1, or the same number of regimes.
 	# Note that the 'values' and 'params' have already been checked to imply this.