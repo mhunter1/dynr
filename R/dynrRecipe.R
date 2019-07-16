e3bbab5c65689e8894509faf66259516f1c8784d
U-MAVIS\mhunter <mhunter.ou@gmail.com>
2019-07-16 16:56:57 -0400

    Clean and homogenize code style for formula dynamics in continuous time

--- R/dynrRecipe.R
+++ R/dynrRecipe.R
@@ -683,89 +683,84 @@ setMethod("writeCcode", "dynrMeasurement",
 # not sure what to do here yet
 setMethod("writeCcode", "dynrDynamicsFormula",
 	function(object, covariates){
-	  formula <- object$formula
-	  jacob <- object$jacobian
-	  nregime=length(formula)
-	  n=sapply(formula,length)
-	  
-	  fml=lapply(formula,processFormula)
-	  lhs=lapply(fml,function(x){lapply(x,"[[",1)})
-	  rhs=lapply(fml,function(x){lapply(x,"[[",2)})
-	  
-	  fmlj=lapply(jacob,processFormula)
-	  row=lapply(fmlj,function(x){lapply(x,"[[",1)})
-	  col=lapply(fmlj,function(x){lapply(x,"[[",2)})
-	  rhsj=lapply(fmlj,function(x){lapply(x,"[[",3)})
-	  
-	  for (i in 1:length(covariates)){
-	    selected <- covariates[i]
-	    get <- paste0("gsl_vector_get(co_variate, ", which(covariates == selected)-1,")")
-	    rhs <- lapply(gsub(paste0("\\<",selected,"\\>"), get, rhs), function(x){eval(parse(text=x))})
-	    rhsj <- lapply(gsub(paste0("\\<",selected,"\\>"), get, rhsj), function(x){eval(parse(text=x))})
-	  }
-	  
-	  if (object@isContinuousTime){
-	    #function_dx_dt
-	    ret="void function_dx_dt(double t, size_t regime, const gsl_vector *x, double *param, size_t n_param, const gsl_vector *co_variate, gsl_vector *F_dx_dt){"
-	    
-	    if (nregime>1){
-	      ret=paste(ret,"switch (regime) {",sep="\n\t")
-	      for (r in 1:nregime){
-	        ret=paste(ret,paste0("\tcase ",r-1,":"),sep="\n\t")
-	        for (i in 1:n[r]){
-	          for (j in 1:length(lhs[[r]])){
-	            rhs[[r]][[i]]=gsub(paste0("\\<",lhs[[r]][[j]],"\\>"),paste0("gsl_vector_get(x,",j-1,")"),rhs[[r]][[i]])
-	          }
-	          ret=paste(ret,paste0("\tgsl_vector_set(F_dx_dt,",i-1,",",rhs[[r]][[i]],");"),sep="\n\t")    
-	        }
-	        ret=paste(ret,paste0("break;\n"),sep="\n\t")
-	        
-	      }
-	      ret=paste(ret,paste0("\t}"),sep="\n\t")
-	      
-	    }else{
-	      for (i in 1:n){
-	        for (j in 1:length(lhs[[1]])){
-	          rhs[[1]][[i]]=gsub(paste0("\\<",lhs[[1]][[j]],"\\>"),paste0("gsl_vector_get(x,",j-1,")"),rhs[[1]][[i]])
-	        }
-	        ret=paste(ret,paste0("\tgsl_vector_set(F_dx_dt,",i-1,",",rhs[[1]][[i]],");"),sep="\n\t")    
-	      }
-	    }
-	    
-	    ret=paste0(ret,"\n\t}")
-	    
-	    #function_dF_dx
-	    ret=paste0(ret,"\n\n/**\n* The dF/dx function\n* The partial derivative of the jacobian of the DE function with respect to the variable x\n* @param param includes at the end the current state estimates in the same order as the states following the model parameters\n*/void function_dF_dx(double t, size_t regime, double *param, const gsl_vector *co_variate, gsl_matrix *F_dx_dt_dx){")
-	    if (nregime>1){
-	      ret=paste(ret,"switch (regime) {",sep="\n\t")
-	      for (r in 1:nregime){
-	        ret=paste(ret,paste0("case ",r-1,":"),sep="\n\t")
-	        for (i in 1:length(jacob[[r]])){
-	          for (j in 1:length(lhs[[r]])){
-	            rhsj[[r]][[i]]=gsub(paste0("\\<",lhs[[r]][[j]],"\\>"),paste0("param[NUM_PARAM+",j-1,"]"),rhsj[[r]][[i]])
-	          }
-	          
-	          ret=paste(ret,paste0("\tgsl_matrix_set(F_dx_dt_dx,",which(lhs[[r]]==row[[r]][[i]])-1,",",which(lhs[[r]]==col[[r]][[i]])-1,",",rhsj[[r]][[i]],");"),sep="\n\t")    
-	        }
-	        ret=paste(ret,paste0("break;\n"),sep="\n\t")
-	        
-	      }
-	      ret=paste(ret,paste0("\t}"),sep="\n\t")
-	      
-	    }else{
-	      for (i in 1:length(jacob[[1]])){
-	        for (j in 1:length(lhs[[1]])){
-	          rhsj[[1]][[i]]=gsub(paste0("\\<",lhs[[1]][[j]],"\\>"),paste0("param[NUM_PARAM+",j-1,"]"),rhsj[[1]][[i]])
-	        }
-	        
-	        ret=paste(ret,paste0("\tgsl_matrix_set(F_dx_dt_dx,",which(unlist(lhs[[1]])==row[[1]][[i]])-1,",",which(unlist(lhs[[1]])==col[[1]][[i]])-1,",",rhsj[[1]][[i]],");"),sep="\n\t")    
-	      }
-	    }
-	    
-	    ret=paste0(ret,"\n\t}")
-	    
-	  }else{
-	    #function_dynam
+		formula <- object$formula
+		jacob <- object$jacobian
+		nregime <- length(formula)
+		n <- sapply(formula,length)
+		
+		fml <- lapply(formula, processFormula)
+		lhs <- lapply(fml, function(x){lapply(x, "[[", 1)})
+		rhs <- lapply(fml, function(x){lapply(x, "[[", 2)})
+		
+		fmlj <- lapply(jacob, processFormula)
+		row <- lapply(fmlj, function(x){lapply(x, "[[", 1)})
+		col <- lapply(fmlj, function(x){lapply(x, "[[", 2)})
+		rhsj <- lapply(fmlj, function(x){lapply(x, "[[", 3)})
+		
+		for (i in 1:length(covariates)){
+			selected <- covariates[i]
+			get <- paste0("gsl_vector_get(co_variate, ", which(covariates == selected)-1, ")")
+			rhs <- lapply(gsub(paste0("\\<", selected, "\\>"), get, rhs), function(x){eval(parse(text=x))})
+			rhsj <- lapply(gsub(paste0("\\<",selected, "\\>"), get, rhsj), function(x){eval(parse(text=x))})
+		}
+		
+		if (object@isContinuousTime){
+			#TODO test if both if(nregime > 1){}else{} breaks (for dx_dt and dF_dx) can be eliminated by using the switch statement with only one regime
+			#function_dx_dt
+			ret <- "void function_dx_dt(double t, size_t regime, const gsl_vector *x, double *param, size_t n_param, const gsl_vector *co_variate, gsl_vector *F_dx_dt){"
+			
+			if (nregime > 1){ #multiple regimes
+				ret <- paste(ret, "switch (regime) {", sep="\n\t")
+				for (r in 1:nregime){
+					ret <- paste(ret, paste0("\tcase ", r-1, ":"), sep="\n\t")
+					for (i in 1:n[r]){
+						for (j in 1:length(lhs[[r]])){
+							rhs[[r]][[i]] <- gsub(paste0("\\<", lhs[[r]][[j]], "\\>"), paste0("gsl_vector_get(x,", j-1, ")"), rhs[[r]][[i]])
+						}
+						ret <- paste(ret, paste0("\tgsl_vector_set(F_dx_dt,", i-1, ",", rhs[[r]][[i]], ");"), sep="\n\t")
+					}
+					ret <- paste(ret, paste0("break;\n"), sep="\n\t")
+				}
+				ret <- paste(ret, paste0("\t}"), sep="\n\t")
+			}else{ #only 1 regime
+				for (i in 1:n){
+					for (j in 1:length(lhs[[1]])){
+						rhs[[1]][[i]] <- gsub(paste0("\\<", lhs[[1]][[j]], "\\>"), paste0("gsl_vector_get(x,", j-1, ")"), rhs[[1]][[i]])
+					}
+					ret <- paste(ret, paste0("\tgsl_vector_set(F_dx_dt,", i-1, ",", rhs[[1]][[i]], ");"), sep="\n\t")
+				}
+			} # end regime ifelse
+			
+			ret <- paste0(ret, "\n\t}")
+			
+			#function_dF_dx
+			ret <- paste0(ret, "\n\n/**\n* The dF/dx function\n* The partial derivative of the jacobian of the DE function with respect to the variable x\n* @param param includes at the end the current state estimates in the same order as the states following the model parameters\n*/void function_dF_dx(double t, size_t regime, double *param, const gsl_vector *co_variate, gsl_matrix *F_dx_dt_dx){")
+			if (nregime > 1){ # more than one regime (regime-switching)
+				ret <- paste(ret, "switch (regime) {", sep="\n\t")
+				for (r in 1:nregime){
+					ret <- paste(ret, paste0("case ", r-1, ":"), sep="\n\t")
+					for (i in 1:length(jacob[[r]])){
+						for (j in 1:length(lhs[[r]])){
+							rhsj[[r]][[i]] <- gsub(paste0("\\<", lhs[[r]][[j]], "\\>"), paste0("param[NUM_PARAM+", j-1, "]"), rhsj[[r]][[i]])
+						}
+						ret <- paste(ret, paste0("\tgsl_matrix_set(F_dx_dt_dx,", which(lhs[[r]] == row[[r]][[i]])-1, ",", which(lhs[[r]] == col[[r]][[i]])-1, ",", rhsj[[r]][[i]], ");"), sep="\n\t")
+					}
+					ret <- paste(ret, paste0("break;\n"), sep="\n\t")
+				}
+				ret <- paste(ret, paste0("\t}"), sep="\n\t")
+			}else{ # else one regime
+				for (i in 1:length(jacob[[1]])){
+					for (j in 1:length(lhs[[1]])){
+						rhsj[[1]][[i]] <- gsub(paste0("\\<", lhs[[1]][[j]], "\\>"), paste0("param[NUM_PARAM+", j-1, "]"), rhsj[[1]][[i]])
+					}
+					ret <- paste(ret, paste0("\tgsl_matrix_set(F_dx_dt_dx,", which(unlist(lhs[[1]]) == row[[1]][[i]])-1, ",", which(unlist(lhs[[1]]) == col[[1]][[i]])-1, ",", rhsj[[1]][[i]], ");"), sep="\n\t")
+				}
+			}
+			
+			ret <- paste0(ret, "\n\t}")
+			
+		} else{ # is Discrete Time
+			#function_dynam
 	    ret="void function_dynam(const double tstart, const double tend, size_t regime, const gsl_vector *xstart,\n\tdouble *param, size_t n_gparam,const gsl_vector *co_variate,\n\tvoid (*g)(double, size_t, const gsl_vector *, double *, size_t, const gsl_vector *, gsl_vector *),\n\tgsl_vector *x_tend){"
 	    
 	    if (nregime>1){
