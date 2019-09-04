32ed2044d67b0c9cb7d728566b34da81fd2a8805
Cynthia Hung <hzh131@aci-lgn-001.acib.production.int.aci.ics.psu.edu>
2019-09-04 12:29:33 -0400

    helloworld of armadillo/superlu both works (add the testing code for armadillo back)

--- src/example1.cpp
+++ src/example1.cpp
@@ -8,6 +8,19 @@
 extern "C" void example1(void);
 void example1(void) {
     printf("hello world from C++\n");
+
+    arma::mat A(2,2);
+
+    A(0,0) = 1;
+    A(0,1) = 0;
+    A(1,0) = 0;
+    A(1,1) = 4;
+    A.print("A");
+
+    A = A % A;
+    A.print("A^2");
+
+
     using namespace arma ;
     sp_mat A2 = sprandu<sp_mat>(4, 4, 0.5);
     A2.zeros();
