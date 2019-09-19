d6c4a282182dba82b254ece98cd7d25bd877c360
unknown <ishtar.hung@gmail.com>
2019-09-18 21:34:57 -0400

    remove helloword files example2*

--- src/example2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int example2(void){
-	printf("hello world!\n");
-	return 0;
-}
--- src/example2.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXAMPLE2    // To make sure you don't declare the function more than once by including the header multiple times.
-#define EXAMPLE2
-
-#include <stdio.h>
-
-int example2(void);
-
-#endif
