26c0055291ec32cabe41d0a8675de56a9bef19fb
unknown <ishtar.hung@gmail.com>
2019-10-04 17:59:55 -0400

    replace with a smaller dataset

--- demo/VDPwithRand.R
+++ demo/VDPwithRand.R
@@ -87,8 +87,8 @@ summary(fitted_model)
 # Check the correlation between estimated b and true b:
 # estimated b in fitted_model@eta_smooth_final
 # true b in the in the 10th column of the input data set
-cor(fitted_model@eta_smooth_final[3,data$tstart[2:201]],
-    vdpData$trueb[data$tstart[2:201]])
+cor(fitted_model@eta_smooth_final[3,data$tstart[2:51]],
+    vdpData$trueb[data$tstart[2:51]])
 
 
 
