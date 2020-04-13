b85c2676b8e473fef3f7069ca11892e1d2fe2d67
Cynthia Hung <ishtar.hung@gmail.com>
2020-04-13 11:21:49 -0400

    change the initial values to true values in the demo file

--- demo/SAEMVDPFix.R
+++ demo/SAEMVDPFix.R
@@ -22,11 +22,11 @@ data <- dynr.data(vdpData, id="id", time="time",
                  covariates=c("u1", "u2"))
 
 meas <- prep.measurement(
-    values.load=matrix(c(1, 1, 1, 0, 0, 0), 3, 2),
+    values.load=matrix(c(1, 0.7, 1.2, 0, 0, 0), 3, 2),
     params.load=matrix(c('fixed', 'lambda_21', 'lambda_31', 'fixed', 'fixed', 'fixed'), 3, 2),
     obs.names = c('y1', 'y2', 'y3'),
     state.names=c('x1', 'x2'),
-    values.int=matrix(c(3, 1, 0), ncol=1),
+    values.int=matrix(c(0, 0, 0), ncol=1),
     params.int=matrix(c('mu1', 'mu2', 'mu3'), ncol=1))
 
 
@@ -46,7 +46,7 @@ initial <- prep.initial(
 mdcov <- prep.noise(
     values.latent=diag(0, 2), 
     params.latent=diag(c("fixed","fixed"), 2),
-    values.observed=diag(rep(0.3,3)),
+    values.observed=diag(rep(0.5,3)),
     params.observed=diag(c("var_1","var_2","var_3"),3)
 )
 
@@ -55,21 +55,21 @@ formula=
          x2 ~ -61.68503 * x1 + zeta_i * (1 - x1^2) * x2
     )
 
-theta.formula  = list (zeta_i ~ 1 * zeta0  + u1 * zeta1 + u2 * zeta2 + 1 * b_zeta)
+theta.formula  = list (zeta_i ~ 1 * zeta0 + u1 * zeta1 + u2 * zeta2 + 1 * b_zeta)
 
 
 
 
 
 dynm<-prep.formulaDynamics(formula=formula,
-                           startval=c(zeta0=-1,
+                           startval=c(zeta0= 3,
                                       zeta1=.5,
-                                      zeta2=.2),
+                                      zeta2=.5),
                            isContinuousTime=TRUE,
                            theta.formula=theta.formula,
                            random.names=c('b_zeta'),
                            random.params.inicov = matrix(c('sigma2_b_zeta'), ncol=1,byrow=TRUE),
-                           random.values.inicov = matrix(c(0.9), ncol=1,byrow=TRUE),
+                           random.values.inicov = matrix(c(0.5), ncol=1,byrow=TRUE),
                            random.lb = -5, 
                            random.ub = 5,
                            saem=TRUE
@@ -95,12 +95,12 @@ print(model@random.params.inicov)
 
 saemp <- prep.saemParameter(MAXGIB = 10, 
                             MAXITER = 10, 
-                            maxIterStage1 = 1005, 
+                            maxIterStage1 = 100, 
                             gainpara = 0.600000, 
                             gainparb = 3.000000, 
                             gainpara1 = 0.900000, 
                             gainparb1 = 1.000000, 
-                            bAdaptParams = c(0.5, 2.5, 0.5),
+                            bAdaptParams = c(0.5, 2.5, 0.5 ,1 ,2, 0.5),
 							KKO=30)
 
 timestart<-Sys.time()
