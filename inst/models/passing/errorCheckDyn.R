0236bb0ae0048c7514fd61a87aab52155cbebed3
U-MAVIS\mhunter <mhunter.ou@gmail.com>
2020-04-24 12:05:39 -0400

    Fix whitespace for dynamics error checking

--- inst/models/passing/errorCheckDyn.R
+++ inst/models/passing/errorCheckDyn.R
@@ -119,76 +119,67 @@ testthat::expect_error(
 # Check conformability of dynamics argument and measurement argument
 
 # Check prep.formulaDynamics formulas have ne left-hand sides (ne: number of latent variables)
-data <- dynr.data(data.frame(id=1,time=1:10,obsy=1:10), id="id", time="time", observed="obsy")
+data <- dynr.data(data.frame(id=1,time=1:10,obsy=1:10),
+	id="id", time="time", observed="obsy")
 
 measurement <- prep.measurement(
-  values.load=matrix(1,1,1), 
-  params.load=matrix("fixed", 1, 1),
-  state.names=c("x"),
-  obs.names=c("obsy")) 
+	values.load=matrix(1,1,1), 
+	params.load=matrix("fixed", 1, 1),
+	state.names=c("x"),
+	obs.names=c("obsy")) 
 
 noise <- prep.noise(
-  values.latent=diag(c(0.1),1), params.latent=diag('noisevar',1), 
-  values.observed=diag(0.1, 1), params.observed=diag('errorvar', 1)) 
+	values.latent=diag(c(0.1),1), params.latent=diag('noisevar',1), 
+	values.observed=diag(0.1, 1), params.observed=diag('errorvar', 1)) 
 
 initial <- prep.initial(
-  values.inistate=c(-2),
-  params.inistate=c('fixed'), 
-  values.inicov=diag(0, 1),
-  params.inicov=diag('fixed', 1))
+	values.inistate=c(-2),
+	params.inistate=c('fixed'), 
+	values.inicov=diag(0, 1),
+	params.inicov=diag('fixed', 1))
 
 # Note: when using the below, there is no error and there should be
 # dynamics <- prep.formulaDynamics(list(x ~ 1, x ~ 1))
-#
-
 testthat::expect_error(
-  dynrmodel <- dynr.model(dynamics, measurement, noise, initial, data),
-  regexp="The number of formulas in each regime in 'prep.formulaDynamics' should match the number of latent states in 'dynrMeasurement'.",
-  fixed=TRUE
-)
-
+	dynrmodel <- dynr.model(dynamics, measurement, noise, initial, data),
+	regexp="The number of formulas in each regime in 'prep.formulaDynamics' should match the number of latent states in 'dynrMeasurement'.",
+	fixed=TRUE)
 # Note: wrong error message thrown.  Is there actually a way to trigger the
 #  above error message?
 
-# Check that prep.formulaDynamics formulas use all and only nne in left-hand sides (nne: names of latent variables)
-formula1D<-list(
-  y~beta*(mu-y)
-)
 
-dynamics<-prep.formulaDynamics(formula=formula1D,startval=c(beta=0.2),
-                               isContinuousTime=TRUE)
+# Check that prep.formulaDynamics formulas use all and only nne in left-hand sides (nne: names of latent variables)
+formula1D <- list( y~beta*(mu-y) )
+
+dynamics <- prep.formulaDynamics(formula=formula1D, startval=c(beta=0.2),
+                                 isContinuousTime=TRUE)
 
 testthat::expect_error(
-  dynrmodel <- dynr.model(dynamics, measurement, noise, initial, data),
-  regexp="The 'state.names' slot of the 'dynrMeasurement' object should match the order of the dynamic formulas specified.",
-  fixed=TRUE
-)
+	dynrmodel <- dynr.model(dynamics, measurement, noise, initial, data),
+	regexp="The 'state.names' slot of the 'dynrMeasurement' object should match the order of the dynamic formulas specified.",
+	fixed=TRUE)
 
 # Check that prep.matrixDynamics is ne by ne
 dynamics <- prep.matrixDynamics(
-  values.dyn=list(vd),
-  params.dyn=list(pd),
-  isContinuousTime=TRUE) 
+	values.dyn=list(vd),
+	params.dyn=list(pd),
+	isContinuousTime=TRUE) 
 
 testthat::expect_error(
-  dynrmodel <- dynr.model(dynamics, measurement, noise, initial, data),
-  regexp="The matrix dimensions in prep.matrixDynamics should match the number of latent states in 'dynrMeasurement'.",
-  fixed=TRUE
-)
+	dynrmodel <- dynr.model(dynamics, measurement, noise, initial, data),
+	regexp="The matrix dimensions in prep.matrixDynamics should match the number of latent states in 'dynrMeasurement'.",
+	fixed=TRUE)
 
 # Check for the correct class of dynamic argument in dynr.model
+testthat::expect_error(
+	dynrmodel <- dynr.model(formula1D, measurement, noise, initial, data),
+	regexp="Check to see that dynamics argument is of the correct class. Hint: it should be either 'dynrDynamicsFormula' or 'dynrDynamicsMatrix'.",
+	fixed=TRUE)
 
 testthat::expect_error(
-  dynrmodel <- dynr.model(formula1D, measurement, noise, initial, data),
-  regexp="Check to see that dynamics argument is of the correct class. Hint: it should be either 'dynrDynamicsFormula' or 'dynrDynamicsMatrix'.",
-  fixed=TRUE
-)
-
-testthat::expect_error(
-  dynrmodel <- dynr.model(measurement,dynamics, noise, initial, data),
-  regexp="Check to see that dynamics argument is of the correct class. Hint: it should be either 'dynrDynamicsFormula' or 'dynrDynamicsMatrix'.",
-  fixed=TRUE
-)
+	dynrmodel <- dynr.model(measurement,dynamics, noise, initial, data),
+	regexp="Check to see that dynamics argument is of the correct class. Hint: it should be either 'dynrDynamicsFormula' or 'dynrDynamicsMatrix'.",
+	fixed=TRUE)
 
 #------------------------------------------------------------------------------
 # End
